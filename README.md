b39df81f6dd434e815e751ad911533a49bcc428b
kefinke <kefinke@mxschool.edu>
2015-01-20 11:03:40 -0500

    added index and README files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kelly Finke
+##### Works at Middlesex
+___
+This is a **super _awesome_** page
+url
+date of deployment
+### Thanks to Justin for giving such beautiful projects to the world
+Change log:
+No changes.
---
+++ index.php
@@ -0,0 +1,9 @@
+<h1>Hello There</h1>
+<p>Welcome this website</p>
+<h3>Reasons this page is great:</h3>
+<ol>
+<li>it's on github (hopefully)</li>
+<li>it has a picture (I think)</li>
+<li>it was made by someone great (check out the README file)</li>
+</ol>
+<img src="https://octodex.github.com/total-eclipse-of-the-octocat/"/>
